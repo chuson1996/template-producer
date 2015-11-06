b786b6d2029afa20b6bc296434c5c51c3657eb3a
CHU SON <chuson1996@gmail.com>
2015-11-06 21:14:57 +0200

    Optimize

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
 # template-producer 
 
 This module requires Node version > 4.0
+
+#Install
+
+NPM: npm install -g template-producer
+
+#Usage
+
+```
+/* File declaring parameters' values */
+/* params.txt */
+{
+	"fi": {
+		"page-title":"Hymy",
+		"title":"Hymy",
+		"image":"http://static4.fjcdn.com/comments/Welcome+to+the+finnish+language+we+hope+you+wont+enjoy+_430d35ecaaedd0521e553557ca0be1c9.png",
+		"caption":"Olet kaunis kun hymyilet."
+	},
+	"vi":{
+		"page-title":"Cười lên nào",
+		"title":"Cười lên nào",
+		"image":"http://www.samoppenheim.com/photos/Vietnam-HoiAn-Boatman.jpg",
+		"caption":"Bạn xinh nhất khi cười."
+	}
+}
+
+/// Note: the file extension doesn't matter, but the content must be of json type
+
+
+/* File template */
+/* smile.temp.html */
+```
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title data-dh-parameter="page-title">Smile</title>
+
+</head>
+<body style="text-align:center">
+	<h1 data-dh-parameter="title">Smile more</h1>
+	<img src="https://smilesolution.files.wordpress.com/2010/07/austin_danger_powers_mike_myers.jpg?w=300&h=259" alt="smile_img" data-dh-parameter="image">
+	<h3 data-dh-parameter="caption">You're beautiful when you smile.</h3>
+</body>
+</html>
+```
+
+Run command: `template-producer smile.temp.html params.txt`
+
+You will see 2 files: smile.fi.html and smile.vi.html where elements' with data-dh-parameter attribute are replaced regarding parameters in params.txt.
--- bin/template-producer.js
+++ bin/template-producer.js
@@ -3,90 +3,101 @@
  */
 'use strict';
 
-
-var jsdom = require("jsdom");
 var fs = require("fs");
 var path = require('path');
 var stripBom = require('strip-bom');
+var jsdom = require('jsdom');
 var root = process.cwd();
-
+var async = require('async');
 
 var jquery = fs.readFileSync(path.resolve(path.dirname(process.argv[1]), "../node_modules/jquery/dist/jquery.js"), {encoding: 'utf8'});
 
-var template = fs.readFileSync(path.resolve(root,process.argv[2]), {encoding: 'utf8'});
-
-
+// console.log(root);
+// console.log(process.argv);
 var paramFile = path.resolve(root,process.argv[3]);
-// console.log(paramFile);
-console.log('Reading params...');
-fs.readFile(paramFile,"utf-8", function (err, data) {
-    if (err) return console.log(err);
-    
-    data = stripBom(data);
-    //console.log(/\u00E4/.test(data));
-    var keys = (data.match(/.+?(?=\|{3})/gm));
-    var vals = data.split('\n').map(function (line) {
-        return line.replace(/.+?\|{3}/,'').trim();
-    });
-    //console.log(vals);
-    var parameters = {};
-    for (var i = 0; i < Math.max(keys.length, vals.length); i++){
-        parameters[keys[i]] = vals[i];
-    }
 
-    console.log("Params: ");
-    console.log(parameters);
+// var outputFile = path.resolve(root, 'output.txt');
+var templateFile = path.resolve(root,process.argv[2]);
+var template = fs.readFileSync(templateFile, {encoding: 'utf8'});
 
-    ///
-    jsdom.env({
-        html: template,
-        src: [jquery],
-        done: function (err, window) {
-            var $ = window.$;
-            var $dhElems = $("*[data-dh-parameter]");
-            //console.log('Number of dhElems: ' + $dhElems.length);
-            //displayDHParameters($dhElems);
-            produceTemplate(parameters);
+async.waterfall([
+    function(cb){
+        fs.readFile(paramFile, "utf8", function(err, data){
+            console.log(data);
+            if (err) return cb(err);
+            try{
+                var jsonContent = JSON.parse(data);
 
-            function displayDHParameters($dhElems){
-                $dhElems.each(function () {
-                    console.log(this.nodeName + ": "+$(this).attr('data-dh-parameter'));
-                    if (this.nodeName=="IMG"){
-                        console.log("-- src=\"" + $(this).attr('src')+"\"");
-                    }else{
-                        console.log("-- innerHTML=\""+ $(this).html()+"\"");
-                    }
+                cb(null, jsonContent);
+            }catch(e){
+                cb(new Error("Invalid parameters!"));
+            }
+        });
+    },
+    function(jsonContent, cb){
+
+        jsdom.env({
+            html: template,
+            src: [jquery],
+            done: function (err, window) {
+                var $ = window.$;
+                var $dhElems = $("*[data-dh-parameter]");
+                //console.log('Number of dhElems: ' + $dhElems.length);
+                //displayDHParameters($dhElems);
+                
+                // First : Language Code
+                Object.keys(jsonContent).forEach(function(languageCode){
+                    // Second: params
+                    produceTemplate(jsonContent[languageCode], templateFile.replace('.temp.html','.'+languageCode+'.html'));
                 })
-            }
 
-            function produceTemplate(parameters){
-
-                Object.keys(parameters).forEach(function (parameter) {
-                    var $queriedElem = $('*[data-dh-parameter="'+parameter+'"]');
-                    if ($queriedElem.length>0){
-                        /* Found! */
-                        if ($queriedElem[0].nodeName == "IMG"){
-                            $queriedElem.attr("src",parameters[parameter]);
+                function displayDHParameters($dhElems){
+                    $dhElems.each(function () {
+                        console.log(this.nodeName + ": "+$(this).attr('data-dh-parameter'));
+                        if (this.nodeName=="IMG"){
+                            console.log("-- src=\"" + $(this).attr('src')+"\"");
                         }else{
-                            $queriedElem.html(parameters[parameter]);
+                            console.log("-- innerHTML=\""+ $(this).html()+"\"");
                         }
-                    }
-                });
+                    })
+                }
 
-                // console.log(paramFile);
-            
-                var newFile = paramFile.replace(/\.param.+/,'.html');
-                // console.log('New file\'s name:',newFile);
-                fs.closeSync(fs.openSync(newFile, 'w'));
+                function produceTemplate(parameters, outputFileName){
 
-                fs.writeFile(newFile, "<!DOCTYPE html><html>"+window.document.querySelector('html').innerHTML+"</html>",{encoding: 'utf8'}, function(err) {
-                    if(err) {
-                        return console.log(err);
-                    }
-                    console.log("The file has been saved!");
-                });
+                    Object.keys(parameters).forEach(function (parameter) {
+                        var $queriedElem = $('*[data-dh-parameter="'+parameter+'"]');
+                        if ($queriedElem.length>0){
+                            /* Found! */
+                            if ($queriedElem[0].nodeName == "IMG"){
+                                $queriedElem.attr("src",parameters[parameter]);
+                            }else{
+                                $queriedElem.html(parameters[parameter]);
+                            }
+                        }
+                    });
+
+                    // console.log(paramFile);
+                
+                    var newFile = outputFileName;
+                    // console.log('New file\'s name:',newFile);
+                    fs.closeSync(fs.openSync(newFile, 'w'));
+
+                    fs.writeFile(newFile, "<!DOCTYPE html><html>"+window.document.querySelector('html').innerHTML+"</html>",{encoding: 'utf8'}, function(err) {
+                        if(err) {
+                            return console.log(err);
+                        }
+                        console.log("The file has been saved!");
+                    });
+                }
             }
-        }
+        });
+
+        
+
+        // fs.writeFile(outputFile, jsonContent, "utf8");
+    }
+    ], function(err, result){
+        if (err) console.error('Error: ',err);
     });
 
-});
+
