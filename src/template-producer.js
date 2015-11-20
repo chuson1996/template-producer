d76b900980683fc3046bd556f20e9f4d0a89fe06
Chu Hoang Son <chuson1996@gmail.com>
2015-11-20 18:39:29 +0200

    Fix bugs ~ JQuery script

--- src/template-producer.js
+++ src/template-producer.js
@@ -12,108 +12,130 @@ var async = require('async');
 
 
 
-
-module.exports = function(options) {
-    options = options || {};
+module.exports = function(props) {
+    props = props || {};
     var root = process.cwd();
-    if (options.pwd) root = path.resolve(root, options.pwd);
-    var jquery = fs.readFileSync(path.resolve(__dirname,"./jquery.min.js"), {
-        encoding: 'utf8'
+    if (props.pwd) root = path.resolve(root, props.pwd);
+    var jquery = fs.readFileSync(path.resolve(path.dirname(__filename),'./jquery.min.js'),"utf8");
+    var argv = process.argv.filter(function(n) {
+        return !n.match(/^--.+/);
     });
-    var paramFile = path.resolve(root, options.parameters || process.argv[3]);
+    var options = process.argv.filter(function(n) {
+        return !!n.match(/^--.+/);
+    });
+    var paramFile = path.resolve(root, props.parameters || argv[3]);
 
     // var outputFile = path.resolve(root, 'output.txt');
-    var templateFile = path.resolve(root, options.template || process.argv[2]);
+    var templateFile = path.resolve(root, props.template || argv[2]);
     var template = fs.readFileSync(templateFile, {
         encoding: 'utf8'
     });
 
-    async.waterfall([
-        function(cb) {
-            fs.readJSON(paramFile, "utf8", function(err, data) {
-                if (err) return cb(err);
-                cb(null, data);
-            });
-        },
-        function(jsonContent, cb) {
+    console.log(options);
+    if (options.indexOf('--watch') !== -1) {
+        console.log('Now watching files...' + paramFile);
+        fs.watchFile(paramFile, function() {
+            console.log('File changed...');
+            start();
+        });
+        console.log('Now watching files...' + templateFile);
+        fs.watchFile(templateFile, function() {
+            console.log('File changed...');
+            start();
+        });
+    }
+    start();
 
-            jsdom.env({
-                html: template,
-                src: [jquery],
-                done: function(err, window) {
-                    var $ = window.$;
-                    var $dhElems = $("*[data-dh-parameter]");
-                    //console.log('Number of dhElems: ' + $dhElems.length);
-                    //displayDHParameters($dhElems);
+    function start() {
+        async.waterfall([
+            function(cb) {
+                fs.readJSON(paramFile, "utf8", function(err, data) {
+                    if (err) return cb(err);
+                    cb(null, data);
+                });
+            },
+            function(jsonContent, cb) {
 
-                    // First : Language Code
-                    Object.keys(jsonContent).forEach(function(writeFile) {
-                        // Second: params
-                        console.log('----------' + writeFile + '-----------')
-                        produceTemplate(jsonContent[writeFile], path.resolve(root, writeFile));
-                    });
+                jsdom.env({
+                    html: template,
+                    src: [jquery],
+                    done: function(err, window) {
+                        var $ = window.$;
+                        var $dhElems = $("*[data-dh-parameter]");
+                        //console.log('Number of dhElems: ' + $dhElems.length);
+                        //displayDHParameters($dhElems);
 
-                    function displayDHParameters($dhElems) {
-                        $dhElems.each(function() {
-                            console.log(this.nodeName + ": " + $(this).attr('data-dh-parameter'));
-                            if (this.nodeName == "IMG") {
-                                console.log("-- src=\"" + $(this).attr('src') + "\"");
-                            } else {
-                                console.log("-- innerHTML=\"" + $(this).html() + "\"");
-                            }
-                        })
-                    }
+                        // First : Language Code
+                        Object.keys(jsonContent).forEach(function(writeFile) {
+                            // Second: params
+                            console.log('----------' + writeFile + '-----------')
+                            produceTemplate(jsonContent[writeFile], path.resolve(root, writeFile));
+                        });
 
-                    function produceTemplate(parameters, outputFileName) {
-
-                        Object.keys(parameters).forEach(function(parameter) {
-                            var $queriedElem = $('*[data-dh-parameter="' + parameter + '"]');
-                            var content = parameters[parameter];
-                            if (typeof content == "object") {
-                                // It's an object with a templateUrl
-                                var templateUrl = path.resolve(root, content.templateUrl);
-                                content = fs.readFileSync(templateUrl, "utf8");
-                            }
-
-                            if ($queriedElem.length > 0) {
-                                /* Found! */
-                                if ($queriedElem[0].nodeName == "IMG") {
-                                    $queriedElem.attr("src", content);
+                        function displayDHParameters($dhElems) {
+                            $dhElems.each(function() {
+                                console.log(this.nodeName + ": " + $(this).attr('data-dh-parameter'));
+                                if (this.nodeName == "IMG") {
+                                    console.log("-- src=\"" + $(this).attr('src') + "\"");
                                 } else {
-                                    $queriedElem.html(content);
+                                    console.log("-- innerHTML=\"" + $(this).html() + "\"");
                                 }
-                                // $queriedElem.removeAttr('data-dh-parameter');
-                            } else {
-                                console.error('Cannot find parameter: ' + parameter);
-                            }
-                        });
+                            })
+                        }
 
-                        // console.log(paramFile);
+                        function produceTemplate(parameters, outputFileName) {
 
-                        var newFile = outputFileName;
-                        process.stdout.write('Writing to ' + outputFileName + '\n');
-                        // console.log('New file\'s name:',newFile);
-                        fs.closeSync(fs.openSync(newFile, 'w'));
+                            Object.keys(parameters).forEach(function(parameter) {
+                                var $queriedElem = $('*[data-dh-parameter="' + parameter + '"]');
+                                var content = parameters[parameter];
+                                if (typeof content == "object") {
+                                    // It's an object with a templateUrl
+                                    var templateUrl = path.resolve(root, content.templateUrl);
+                                    content = fs.readFileSync(templateUrl, "utf8");
+                                    
+                                }
 
-                        fs.writeFile(newFile, "<!DOCTYPE html><html>" + window.document.querySelector('html').innerHTML + "</html>", {
-                            encoding: 'utf8'
-                        }, function(err) {
-                            if (err) {
-                                return console.log(err);
-                            }
-                            console.log("The file has been saved!");
-                        });
+                                if ($queriedElem.length > 0) {
+                                    /* Found! */
+                                    if ($queriedElem[0].nodeName == "IMG") {
+                                        $queriedElem.attr("src", content);
+                                    } else {
+                                        $queriedElem.html(content);
+                                    }
+                                    // $queriedElem.removeAttr('data-dh-parameter');
+                                } else {
+                                    console.error('Cannot find parameter: ' + parameter);
+                                }
+                            });
+
+                            // console.log(paramFile);
+
+                            var newFile = outputFileName;
+                            process.stdout.write('Writing to ' + outputFileName + '\n');
+                            // console.log('New file\'s name:',newFile);
+                            fs.closeSync(fs.openSync(newFile, 'w'));
+
+                            fs.writeFile(newFile, "<!DOCTYPE html><html>" + window.document.querySelector('html').innerHTML + "</html>", {
+                                encoding: 'utf8'
+                            }, function(err) {
+                                if (err) {
+                                    return console.log(err);
+                                }
+                                console.log("The file has been saved!");
+                            });
+                        }
                     }
-                }
-            });
+                });
 
 
 
-            // fs.writeFile(outputFile, jsonContent, "utf8");
-        }
-    ], function(err, result) {
-        if (err) console.error('Error: ', err);
-    });
+                // fs.writeFile(outputFile, jsonContent, "utf8");
+            }
+        ], function(err, result) {
+            if (err) console.error('Error: ', err);
+        });
+    }
+
 
 
     function cleanJSON(string) {
